166dd004b4e6cbece05e64c3fb9858efc3f79e45
Brianna Jordan <bryvjordan9@gmail.com>
2019-10-03 22:46:06 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -5,23 +5,33 @@ class Song
   @@artists = []
   @@genres = []
 
-
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    @@count += 1
-    @@artists << artist
+    @@count +=1
     @@genres << genre
+    @@artists << artist
   end
 
   def self.count
     @@count
-
   end
 
   def self.artists
     @@artists.uniq
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
   end
 
   def self.genres
@@ -29,6 +39,14 @@ class Song
   end
 
   def self.genre_count
-    genre_count.each do |genres|
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
   end
 end
