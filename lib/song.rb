6cbd8d5b9cf03cd8c15d690c02bdd1b7899c3c4c
Jalysa Reason <jalysareason@gmail.com>
2019-10-15 01:48:48 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,6 @@
+class Song
+
+  let!(:lucifer) { Song.new("Lucifer", "Jay-Z", "rap" ) }
+  let!(:ninety_nine_problems) { Song.new("99 Problems", "Jay-Z", "rap") }
+  let!(:hit_me) { Song.new("hit me baby one more time", "Brittany Spears", "pop") }
+end
